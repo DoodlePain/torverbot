56b68f40a61bb53ba9b39a4daba4949ef6572714
Manuel Scarapazzi <manuelfrulloscarapazzi@gmail.com>
2018-02-19 14:12:40 +0100

    orario fix

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ bot.on('/menu', msg =>{
       ],
       ['/orari']
     ], {resize: true});
-    return bot.sendMessage(msg.from.id, 'Benvenuto nel bot di Tor Vergata, il modo piu\' semplice per ricevere informazioni relative al corso di Informatica', {replyMarkup});
+    return bot.sendMessage(msg.from.id, 'Qui trovi tutto quello che cerchi.', {replyMarkup});
 });
 
 
@@ -52,20 +52,211 @@ bot.on('/primo', msg => {
   request({
     uri: "http://informatica.uniroma2.it/pages/trien/orario/orario.htm"
   }, function(error, response, body) {
-    body = body.replace(/&nbsp;/gi, " ")
-    body = body.replace('null', ' ')
-    body = body.split("\"><td align=right>")
-    b = body
+    var orario=[];
+    body = body. split("00</td>");
+    var app  = [];
+    var app2  = [];
+    var app3  = [];
+    var app4  = [];
+    var app5  = [];
+    var ore = ["9:00 : ","10:00 : ","11:00 : ","12:00 : ","13:00 : ","14:00 : ","15:00 : ","16:00 : ","17:00 : ","18:00 : "]
+      for(i = 1;i<11;i++){
+        bodya = body[i].split("</tr><tr style=\"")
+        bodya = bodya[0];
+        bodya = bodya.split("<td");
+        body1 = bodya[1];
+        body1 = body1.split(">");
+        if(body1[2]!=''){
+          body1 = "\n" + ore[i-1]+"" +body1[2].replace("</a","");
+        } else {
+          body1 = "\n" + ore[i-1]+"" + "Nulla"
+        }
+        app.push(body1);
+        body2 = bodya[2];
+        body2 = body2.split(">");
+        if(body2[2]!=''){
+          body2 = "\n" + ore[i-1]+"" +body2[2].replace("</a","");
+        } else {
+          body2 = "\n" + ore[i-1]+"" + "Nulla"
+        }
+        app2.push(body2);
+        body3 = bodya[3];
+        body3 = body3.split(">");
+        if(body3[2]!=''){
+          body3 = "\n" + ore[i-1]+"" +body3[2].replace("</a","");
+        } else {
+          body3 = "\n" + ore[i-1]+"" + "Nulla"
+        }
+        app3.push(body3);
+        body4 = bodya[4];
+        body4 = body4.split(">");
+        if(body4[2]!=''){
+          body4 = "\n" + ore[i-1]+"" +body4[2].replace("</a","");
+        } else {
+          body4 = "\n" + ore[i-1]+"" + "Nulla"
+        }
+        app4.push(body4);
+        body5 = bodya[5];
+        body5 = body5.split(">");
+        if(body5[2]!=''){
+          body5 = "\n" + ore[i-1]+"" +body5[2].replace("</a","");
+        } else {
+          body5 = "\n" + ore[i-1]+"" + "Nulla"
+        }
+        app5.push(body5);
+      }
+    orario.push(app,app2,app3,app4,app5);
+    bot.sendMessage(msg.from.id,"Lunedi' :\n"+orario[0]).then(()=>{
+      return bot.sendMessage(msg.from.id,"Martedi' :\n"+orario[1]).then(()=>{
+        return bot.sendMessage(msg.from.id,"Mercoledi' :\n"+orario[2]).then(()=>{
+          return bot.sendMessage(msg.from.id,"Giovedi' :\n"+orario[3]).then(()=>{
+            return bot.sendMessage(msg.from.id,"Venerdi' :\n"+orario[4])
+          })
+        })
+      })
+    })
+  })
+});
 
-    i = 1;
-    while(b[i]!=undefined){
-      body = b[i].split("</td>")
+// Orario del secondo
+bot.on('/secondo', msg => {
+  request({
+    uri: "http://informatica.uniroma2.it/pages/trien/orario/orario.htm"
+  }, function(error, response, body) {
+    var orario=[];
+    body = body. split("00</td>");
+    var app  = [];
+    var app2  = [];
+    var app3  = [];
+    var app4  = [];
+    var app5  = [];
+    var ore = ["9:00 : ","10:00 : ","11:00 : ","12:00 : ","13:00 : ","14:00 : ","15:00 : ","16:00 : ","17:00 : ","18:00 : "]
+      for(i = 11;i<21;i++){
+        bodya = body[i].split("</tr><tr style=\"")
+        bodya = bodya[0];
+        bodya = bodya.split("<td");
+        body1 = bodya[1];
+        body1 = body1.split(">");
+        if(body1[2]!=''){
+          body1 = "\n" + ore[i-11]+"" +body1[2].replace("</a","");
+        } else {
+          body1 = "\n" + ore[i-11]+"" + "Nulla"
+        }
+        app.push(body1);
+        body2 = bodya[2];
+        body2 = body2.split(">");
+        if(body2[2]!=''){
+          body2 = "\n" + ore[i-11]+"" +body2[2].replace("</a","");
+        } else {
+          body2 = "\n" + ore[i-11]+"" + "Nulla"
+        }
+        app2.push(body2);
+        body3 = bodya[3];
+        body3 = body3.split(">");
+        if(body3[2]!=''){
+          body3 = "\n" + ore[i-11]+"" +body3[2].replace("</a","");
+        } else {
+          body3 = "\n" + ore[i-11]+"" + "Nulla"
+        }
+        app3.push(body3);
+        body4 = bodya[4];
+        body4 = body4.split(">");
+        if(body4[2]!=''){
+          body4 = "\n" + ore[i-11]+"" +body4[2].replace("</a","");
+        } else {
+          body4 = "\n" + ore[i-11]+"" + "Nulla"
+        }
+        app4.push(body4);
+        body5 = bodya[5];
+        body5 = body5.split(">");
+        if(body5[2]!=''){
+          body5 = "\n" + ore[i-11]+"" +body5[2].replace("</a","");
+        } else {
+          body5 = "\n" + ore[i-11]+"" + "Nulla"
+        }
+        app5.push(body5);
+      }
+    orario.push(app,app2,app3,app4,app5);
+    bot.sendMessage(msg.from.id,"Lunedi' :\n"+orario[0]).then(()=>{
+      return bot.sendMessage(msg.from.id,"Martedi' :\n"+orario[1]).then(()=>{
+        return bot.sendMessage(msg.from.id,"Mercoledi' :\n"+orario[2]).then(()=>{
+          return bot.sendMessage(msg.from.id,"Giovedi' :\n"+orario[3]).then(()=>{
+            return bot.sendMessage(msg.from.id,"Venerdi' :\n"+orario[4])
+          })
+        })
+      })
+    })
+  })
+});
 
-      body = body[0]
-      console.log(striptags(body));
-      i++
-    }
-
+// Orario del terzo
+bot.on('/terzo', msg => {
+  request({
+    uri: "http://informatica.uniroma2.it/pages/trien/orario/orario.htm"
+  }, function(error, response, body) {
+    var orario=[];
+    body = body. split("00</td>");
+    var app  = [];
+    var app2  = [];
+    var app3  = [];
+    var app4  = [];
+    var app5  = [];
+    var ore = ["9:00 : ","10:00 : ","11:00 : ","12:00 : ","13:00 : ","14:00 : ","15:00 : ","16:00 : ","17:00 : ","18:00 : "]
+      for(i = 21;i<31;i++){
+        bodya = body[i].split("</tr><tr style=\"")
+        bodya = bodya[0];
+        bodya = bodya.split("<td");
+        body1 = bodya[1];
+        body1 = body1.split(">");
+        if(body1[2]!=''){
+          body1 = "\n" + ore[i-21]+"" +body1[2].replace("</a","");
+        } else {
+          body1 = "\n" + ore[i-21]+"" + "Nulla"
+        }
+        app.push(body1);
+        body2 = bodya[2];
+        body2 = body2.split(">");
+        if(body2[2]!=''){
+          body2 = "\n" + ore[i-21]+"" +body2[2].replace("</a","");
+        } else {
+          body2 = "\n" + ore[i-21]+"" + "Nulla"
+        }
+        app2.push(body2);
+        body3 = bodya[3];
+        body3 = body3.split(">");
+        if(body3[2]!=''){
+          body3 = "\n" + ore[i-21]+"" +body3[2].replace("</a","");
+        } else {
+          body3 = "\n" + ore[i-21]+"" + "Nulla"
+        }
+        app3.push(body3);
+        body4 = bodya[4];
+        body4 = body4.split(">");
+        if(body4[2]!=''){
+          body4 = "\n" + ore[i-21]+"" +body4[2].replace("</a","");
+        } else {
+          body4 = "\n" + ore[i-21]+"" + "Nulla"
+        }
+        app4.push(body4);
+        body5 = bodya[5];
+        body5 = body5.split(">");
+        if(body5[2]!=''){
+          body5 = "\n" + ore[i-21]+"" +body5[2].replace("</a","");
+        } else {
+          body5 = "\n" + ore[i-21]+"" + "Nulla"
+        }
+        app5.push(body5);
+      }
+    orario.push(app,app2,app3,app4,app5);
+    bot.sendMessage(msg.from.id,"Lunedi' :\n"+orario[0]).then(()=>{
+      return bot.sendMessage(msg.from.id,"Martedi' :\n"+orario[1]).then(()=>{
+        return bot.sendMessage(msg.from.id,"Mercoledi' :\n"+orario[2]).then(()=>{
+          return bot.sendMessage(msg.from.id,"Giovedi' :\n"+orario[3]).then(()=>{
+            return bot.sendMessage(msg.from.id,"Venerdi' :\n"+orario[4])
+          })
+        })
+      })
+    })
   })
 });
 
