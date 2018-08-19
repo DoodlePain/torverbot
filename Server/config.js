0ccdc7625f29bc5b775fa502fb45059b6ea538fc
Manuel Scarapazzi <manuelfrulloscarapazzi@gmail.com>
2018-08-19 02:44:58 +0200

    Update config.js

--- Server/config.js
+++ Server/config.js
@@ -1,4 +1,4 @@
-const accessToken = '371457888:AAFPcUPqD8ki1vPOEem8P75L1pZdpBbuaCc';
+const accessToken = 'Your-TOKEN-Here';
 const TeleBot = require('telebot');
 const bot = new TeleBot(accessToken);
 
